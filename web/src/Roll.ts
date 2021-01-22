74469f5d05a0769cee1140e24610929e97203587
Frank <frankvd@users.noreply.github.com>
2021-01-22 20:50:43 +0100

    Update web styling

--- web/src/Roll.ts
+++ web/src/Roll.ts
@@ -2,47 +2,73 @@ import { LitElement, property, html, css } from "lit-element"
 import Config from "./Config"
 
 export class Roll extends LitElement {
+  @property({type: String}) title = ''
   @property({type: String}) definition = ''
-  @property({type: String}) output = ''
+  @property({type: String}) output = '... ... ... ... ... ... ... ... ... ... ... ...'
 
   static get styles() {
     return css`
       .roll-container {
-        display: grid;
+        /* display: grid;
         grid-template-columns: auto 80px;
-        grid-template-rows: 40px 40px;
-        grid-auto-flow: column;
-        background: #111;
+        grid-template-rows: 25px 60px 40px;
+        grid-auto-flow: column; */
+        background: #fff;
+        margin: 24px 0;
+        border: 30px solid #FFFFFF;
+        border-radius: 30px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        overflow: auto;
+        width: 420px;
       }
 
+      .roll-title {
+        font-family: Roboto Slab;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 24px;
+        line-height: 32px;
+        color: #000000;
+      }
+      .roll-menu-button {
+        float: right;
+      }
       .roll-button {
-        background: #111;
-        color: var(--fg);
-        border: 0;
-        border-left: 1px solid var(--fg);
-        border-radius: 0;
-        height: 100%;
-        width: 100%;
-        text-align: center;
-        grid-row-start: 1;
-        grid-row-end: 3;
-        outline: none;
+        font-size: 18px;
+        line-height: 21px;
+        text-transform: uppercase;
+        font-feature-settings: 'cpsp' on;
+        color: #000;
+        float:right;
+        text-decoration: none;
+        margin: 12px;
         cursor: pointer;
       }
 
+      .roll-definition {
+        font-size: 24px;
+        line-height: 28px;
+        margin: 20px 0;
+      }
+
       .roll-input {
-        background: var(--fg);
+        background: var(--bg);
         border: 0;
         border-radius: 0;
         outline: none;
       }
 
       .roll-output {
-        line-height: 40px;
-        padding: 0 6px;
-      }
-      .roll-output::before {
-        content: "> "
+        background: #000000;
+        padding: 6px;
+        clip-path: polygon(6px 0%, calc(100% - 6px) 0%, 100% 6px, 100% calc(100% - 6px), calc(100% - 6px) 100%, 6px 100%, 0% calc(100% - 6px), 0% 6px);
+        font-family: Roboto Mono;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 14px;
+        box-sizing: border-box;
+        height: 30px;
+        color: #fff;
       }
     `
   }
@@ -50,11 +76,15 @@ export class Roll extends LitElement {
   render() {
     return html`
       <div class="roll-container">
-        <input class="roll-input" value="${this.definition}" @change=${(e:any) => {this.definition = e.target.value;console.log(e)}} />
+        <div class="roll-menu-button">...</div>
+        <div class="roll-title">Shortsword +1</div>
+        <div class="roll-definition">${this.definition}</div>
         <div class="roll-output">${this.output}</div>
-        <button class="roll-button" @click="${this.roll}">Roll</button>
+        <a class="roll-button" @click="${this.roll}">Roll</a>
       </div>
     `
+
+    //  <input class="roll-input" value="${this.definition}" @change=${(e:any) => {this.definition = e.target.value;}} />
   }
 
   roll() {
